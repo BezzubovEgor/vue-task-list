d315f7387b7a4f48bed2676f854e4b83743384e5
bezzubov_egor <bezzubov.egor@gmail.com>
2019-05-22 09:18:10 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fde20fedf6b4ab71cd8d779d4504150c.js"
+  "/precache-manifest.970853e07610a8d22f1dfa8d8bc8f156.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-todo"});
