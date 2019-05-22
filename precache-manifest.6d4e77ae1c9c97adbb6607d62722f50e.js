3001f679a000229842bea32f969b4bba28a568ff
bezzubov_egor <bezzubov.egor@gmail.com>
2019-05-22 09:40:02 +0300

    Updates

---
+++ precache-manifest.6d4e77ae1c9c97adbb6607d62722f50e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "14aac393f50a1c9d7651",
+    "url": "/vue-task-list/css/app.a53d305f.css"
+  },
+  {
+    "revision": "14aac393f50a1c9d7651",
+    "url": "/vue-task-list/js/app.7060e3b7.js"
+  },
+  {
+    "revision": "5ce5e9c68bd2f92a4a54",
+    "url": "/vue-task-list/js/chunk-vendors.904c332c.js"
+  },
+  {
+    "revision": "d043e42ecbf27a0737a6e341d33f2316",
+    "url": "/vue-task-list/img/projects.d043e42e.svg"
+  },
+  {
+    "revision": "75cfc88eb2cf654e87c355e43a3c8570",
+    "url": "/vue-task-list/img/todo.75cfc88e.svg"
+  },
+  {
+    "revision": "cd81139ea54a5a49a4a2539f6ebcd828",
+    "url": "/vue-task-list/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-task-list/robots.txt"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.970853e07610a8d22f1dfa8d8bc8f156.js"
+  "/vue-task-list/precache-manifest.6d4e77ae1c9c97adbb6607d62722f50e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-todo"});
