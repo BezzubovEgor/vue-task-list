92b6ea5a41b9595c8385dfd7f43e6a7271d4fad9
bezzubov_egor <bezzubov.egor@gmail.com>
2019-05-22 07:30:23 +0300

    Updates

---
+++ precache-manifest.d27d3caeb3a3e1781fd39c6518c6714b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5ce5e9c68bd2f92a4a54",
+    "url": "/js/chunk-vendors.904c332c.js"
+  },
+  {
+    "revision": "267c044c7223255aec84",
+    "url": "/js/app.43dcc2bd.js"
+  },
+  {
+    "revision": "d8ade37fe3820c3250e9207daa62f39d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75cfc88eb2cf654e87c355e43a3c8570",
+    "url": "/img/todo.75cfc88e.svg"
+  },
+  {
+    "revision": "d043e42ecbf27a0737a6e341d33f2316",
+    "url": "/img/projects.d043e42e.svg"
+  },
+  {
+    "revision": "267c044c7223255aec84",
+    "url": "/css/app.0aaa2ea5.css"
+  }
+];
